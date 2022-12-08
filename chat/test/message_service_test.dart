f386b8bf427faac396f2d3feae07fa18054ba49f
Vidit Khandelwal <vidit8290@gmail.com>
2022-12-08 15:42:08 +0530

    Added services of user,message and receipt

---
+++ chat/test/message_service_test.dart
@@ -0,0 +1,97 @@
+import 'package:chat/src/models/message.dart';
+import 'package:chat/src/models/user.dart';
+import 'package:chat/src/services/encryption/encryption_service_impl.dart';
+import 'package:chat/src/services/message/message_service_impl.dart';
+import 'package:encrypt/encrypt.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rethink_db_ns/rethink_db_ns.dart';
+
+import 'helpers.dart';
+
+void main() {
+  RethinkDb r = RethinkDb();
+  Connection? connection;
+  MessageService? sut;
+
+  setUp(() async {
+    connection = await r.connect(host: 'localhost', port: 28015);
+    final encryption = EncryptionService(Encrypter(AES(Key.fromLength(32))));
+    await createDb(r, connection!);
+    sut = MessageService(r, connection!, encryption);
+  });
+
+  tearDown(() async {
+    sut?.dispose();
+    await cleanDb(r, connection!);
+  });
+
+  final user = User.fromJson({
+    'id': '1234',
+    'active': true,
+    'lastSeen': DateTime.now(),
+  });
+
+  final user2 = User.fromJson({
+    'id': '1111',
+    'active': true,
+    'lastSeen': DateTime.now(),
+  });
+
+  test('message sent successfully', () async {
+    Message message = Message(
+        from: user.id,
+        to: '3456',
+        timestamp: DateTime.now(),
+        contents: 'this is a test message');
+
+    final res = await sut?.send(message);
+    expect(res, true);
+  });
+
+  test('successfully subscribe and receive messages', () async {
+    const contents = 'this is a message';
+    sut?.messages(activeUser: user2).listen(expectAsync1((message) {
+          expect(message.to, user2.id);
+          expect(message.id, isNotEmpty);
+          expect(message.contents, contents);
+        }, count: 2));
+
+    Message message = Message(
+        from: user.id,
+        to: user2.id,
+        timestamp: DateTime.now(),
+        contents: contents);
+
+    Message message2 = Message(
+        from: user.id,
+        to: user2.id,
+        timestamp: DateTime.now(),
+        contents: contents);
+
+    await sut?.send(message);
+    await sut?.send(message2);
+  });
+
+  test('successfully subscribe and receive new messages', () async {
+    Message message = Message(
+        from: user.id,
+        to: user2.id,
+        timestamp: DateTime.now(),
+        contents: 'this is a test message');
+
+    Message secondMessage = Message(
+        from: user.id,
+        to: user2.id,
+        timestamp: DateTime.now(),
+        contents: 'this is a another test message');
+
+    await sut?.send(message);
+    await sut?.send(secondMessage).whenComplete(
+          () => sut?.messages(activeUser: user2).listen(
+                expectAsync1((message) {
+                  expect(message.to, user2.id);
+                }, count: 2),
+              ),
+        );
+  });
+}
